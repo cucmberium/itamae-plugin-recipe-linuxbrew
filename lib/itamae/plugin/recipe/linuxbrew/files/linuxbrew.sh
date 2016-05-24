451421afaefe2e1bdb9f87cb9595ea903b090d1e
iyuuya <i.yuuya@gmail.com>
2016-05-24 19:57:47 +0900

    add $HOME/.linuxbrew/sbin to PATH

--- lib/itamae/plugin/recipe/linuxbrew/files/linuxbrew.sh
+++ lib/itamae/plugin/recipe/linuxbrew/files/linuxbrew.sh
@@ -1,5 +1,5 @@
 if [ -d "$HOME/.linuxbrew" ]; then
-  export PATH="$HOME/.linuxbrew/bin:$PATH"
+  export PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
   export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
   export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
 fi
